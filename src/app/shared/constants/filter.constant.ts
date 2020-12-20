846a26f068de8aa937bb0a110daa365923af12a2
Ashish Kumar <ashish.kumar@moonraft.com>
2020-12-20 20:53:09 +0530

    constants for filter added

---
+++ src/app/shared/constants/filter.constant.ts
@@ -0,0 +1,8 @@
+
+const YEARS_RANGE = [2006, 2020];
+const years = [];
+for (let i = YEARS_RANGE[0]; i <= YEARS_RANGE[1]; i++){
+    years.push(i);
+}
+export const YEARS = years;
+export const LAUNCH_OPTIONS = [true, false];
