9f36c34285998e19926900e0f6b5687dae4ae695
Ashish Kumar <ashish.kumar@moonraft.com>
2020-12-20 20:54:12 +0530

    launch item input added

--- src/app/shared/components/launch-item/launch-item.component.ts
+++ src/app/shared/components/launch-item/launch-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LaunchModel } from '../../models/launch.model';
 
 @Component({
   selector: 'spacex-launch-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./launch-item.component.css']
 })
 export class LaunchItemComponent implements OnInit {
-
+  @Input('data') launchData: LaunchModel;
   constructor() { }
 
   ngOnInit(): void {
